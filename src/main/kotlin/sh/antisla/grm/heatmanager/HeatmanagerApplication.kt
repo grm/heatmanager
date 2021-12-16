50db3bc5d09e1c86455508d40b1109c7140e10eb
Jérémie Klein <jklein@octo.com>
2021-12-16 22:40:36 +0100

    App skeleton

---
+++ src/main/kotlin/sh/antisla/grm/heatmanager/HeatmanagerApplication.kt
@@ -0,0 +1,11 @@
+package sh.antisla.grm.heatmanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HeatmanagerApplication
+
+fun main(args: Array<String>) {
+	runApplication<HeatmanagerApplication>(*args)
+}
---
+++ src/test/kotlin/sh/antisla/grm/heatmanager/HeatmanagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package sh.antisla.grm.heatmanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HeatmanagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
